40fbe89fa50150f28af6f683b27475e8b4d7c1fa
jimenezperezpablo <jimenezperezpablo001.alu@stemgranada.com>
2026-01-08 19:33:20 +0100

    Modifico ejercicio 5

--- Apuntes/Apuntes_Programacion/Ejercicios/TEMA_4/ejercicios/UD1/R1/Ejercicio5.java
+++ Apuntes/Apuntes_Programacion/Ejercicios/TEMA_4/ejercicios/UD1/R1/Ejercicio5.java
@@ -1,5 +1,7 @@
 package ejercicios.UD1.R1;
+
 import java.util.Scanner;
+
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -7,8 +9,9 @@ public class Ejercicio5 {
         // Leer datos del usuario
         System.out.print("Ingresa tu peso: ");
         double peso = scanner.nextDouble();
+        double pesoLuna = peso * 0.165;
 
-        System.out.println("Su peso en la luna es: " + peso * 0.165); 
+        System.out.println("Su peso en la luna es: " + pesoLuna + " kg");
         scanner.close();
     }
 }
