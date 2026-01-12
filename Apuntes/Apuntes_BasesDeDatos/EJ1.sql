e98123006863e9655fc8008e89d3b7e56819ddbd
jimenezperezpablo <jimenezperezpablo001.alu@stemgranada.com>
2026-01-12 15:09:39 +0100

    SQL Ejercicio 1

--- Apuntes/Apuntes_BasesDeDatos/EJ1.sql
+++ Apuntes/Apuntes_BasesDeDatos/EJ1.sql
@@ -6,4 +6,4 @@ SELECT nombre, ciudad FROM techcorp_db.empleados;
 SELECT nombre SELECT techcorp_db.empleados WHERE departamento = "marketing" || departamento = "ventas" && salario > 5000;
 select nombre from techcorp_db.empleados where departamento != "recursos humanos";
 SELECT nombre FROM techcorp_db.clientes where ciudad = "barcelona";
-çSELECT nombre FROM techcorp_db.empleados where edad < 30 and salario > 4000;
+SELECT nombre FROM techcorp_db.empleados where edad < 30 and salario > 4000;
