1bcdc62c920103291c7ff619518a4ffbb938e135
jimenezperezpablo <jimenezperezpablo001.alu@stemgranada.com>
2026-03-09 15:45:44 +0100

    Subo relacion programacion Tema 6 rel 1

---
+++ Apuntes/Apuntes_Programacion/Ejercicios/TEMA_6/Relación1/Ejercicio2/Empleado.java
@@ -0,0 +1,21 @@
+public class Empleado {
+    private String nombre;
+    private double sueldoBase;
+    Empleado(String nombre, double sueldoBase) {
+        this.nombre = nombre;
+        this.sueldoBase = sueldoBase;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+    
+}
---
+++ Apuntes/Apuntes_Programacion/Ejercicios/TEMA_6/Relación1/Ejercicio2/Main.java
@@ -0,0 +1,13 @@
+// Crea una clase Empleado con los atributos nombre y sueldoBase. 
+// Luego, crea tres subclases: Programador, Diseñador y Gerente. 
+// Cada subclase debe tener un método calcularSueldo() 
+// que ajuste el sueldo base según un bonificador específico para cada tipo de empleado.
+// private final double BONIFICADOR = 1.5
+
+// Implementa una clase Main para probar la creación de objetos y la llamada al método calcularSueldo().
+
+public class Main{
+    public static void main(String[] args) {
+      
+    }
+}
